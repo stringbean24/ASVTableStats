5a6c981a007c9df6630ac0cdd279badbf8fbca39
stringbean24 <bdw9157@rit.edu>
2025-05-05 16:05:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+## `setup.py`
 
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name='qiime2-seqlen',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'qiime2',
+        'pandas',
+        'biopython',
+        'seaborn',
+        'matplotlib',
+        'numpy'
+    ],
+    entry_points={
+        'qiime2.plugins': ['seqlen=seqlen_plugin.plugin_setup:plugin']
+    }
+)
